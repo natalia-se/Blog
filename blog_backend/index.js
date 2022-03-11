2c783cf47fe43726540c671d77b33e41979b2864
natalia-se <flaunder1000@gmail.com>
2022-03-11 19:15:37 +0100

    Add registration endpoint

--- blog_backend/index.js
+++ blog_backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRout = require("./routes/auth");
 
 dotenv.config();
 
@@ -9,6 +10,9 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("mongo is running"))
   .catch((err) => console.log(err));
+
+app.use("/api/auth", authRout);
+
 app.use("/", (req, res) => {
   console.log("Home page");
 });
--- blog_backend/models/User.js
+++ blog_backend/models/User.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  id: { type: String, unique: true },
   username: { type: String, required: true, unique: true },
   email: { type: String, required: false },
   password: { type: String, required: true },
   fullname: String,
 });
 
-exports.User = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);

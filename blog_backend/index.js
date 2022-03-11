8268c258226555353df86e927189584091d8d054
natalia-se <flaunder1000@gmail.com>
2022-03-11 17:16:52 +0100

    Use mongoose

--- blog_backend/index.js
+++ blog_backend/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("mongo is running"))
+  .catch((err) => console.log(err));
 app.use("/", (req, res) => {
   console.log("Home page");
 });

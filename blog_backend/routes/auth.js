0ef39766c54a5f7db9c9e67af09681b36aaf6473
natalia-se <flaunder1000@gmail.com>
2022-03-12 10:35:40 +0100

    Add login endpoint

--- blog_backend/routes/auth.js
+++ blog_backend/routes/auth.js
@@ -17,6 +17,29 @@ router.post("/register", async (req, res) => {
     const user = await newUser.save();
 
     res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Login
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    console.log(user);
+    if (!user) {
+      res.status(400).json("Wrong credentials!");
+      return;
+    }
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    if (!validated) {
+      res.status(400).json("Wrong credentials!");
+      return;
+    }
+
+    const { password, __v, ...rest } = user._doc;
+    res.status(200).json(rest);
   } catch (err) {
     res.status(500).json(err);
   }

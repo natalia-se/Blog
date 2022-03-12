0abdb58c36793d74af44573bf5e674b1a581afd3
natalia-se <flaunder1000@gmail.com>
2022-03-12 15:08:15 +0100

    Add update message endpoint

--- blog_backend/routes/messages.js
+++ blog_backend/routes/messages.js
@@ -12,4 +12,33 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Update post
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const user_id = req.body.user_id;
+
+  try {
+    const message = await Message.findById(id);
+
+    if (message.user_id.toString() === user_id) {
+      try {
+        const updatedMessage = await Message.findByIdAndUpdate(
+          id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedMessage);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    } else {
+      res.status(401).json("You can update only your message");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;

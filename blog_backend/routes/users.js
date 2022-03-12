62715750d9f69c8231ed491de774ff29d21ae201
natalia-se <flaunder1000@gmail.com>
2022-03-12 13:31:53 +0100

    Add delete user endpoint

--- blog_backend/routes/users.js
+++ blog_backend/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Message = require("../models/Message");
 const bcrypt = require("bcrypt");
 
 // Update
@@ -24,7 +25,31 @@ router.put("/:id", async (req, res) => {
       res.status(500).json(error);
     }
   } else {
-    res.status(401).json("You can only update your account");
+    res.status(401).json("You can update only your account");
+  }
+});
+
+// Delete
+router.delete("/:id", async (req, res) => {
+  const { id } = req.body;
+
+  if (id === req.params.id) {
+    try {
+      const user = await User.findById(id);
+
+      try {
+        await Message.deleteMany({ user_id: id });
+        await User.findByIdAndDelete(id);
+        res.status(200).json("User has been deleted");
+      } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+      }
+    } catch (error) {
+      res.status(404).json("User not found!");
+    }
+  } else {
+    res.status(401).json("You can delete only your account");
   }
 });
 

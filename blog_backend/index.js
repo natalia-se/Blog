dbb19ee930447fd99413f43060b5916769f0da44
natalia-se <flaunder1000@gmail.com>
2022-03-13 23:52:55 +0100

    Add upload image endpoint

--- blog_backend/index.js
+++ blog_backend/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const { authRoute, authorization, requireLogin } = require("./routes/auth");
 const userRoute = require("./routes/users");
 const messageRoute = require("./routes/messages");
+const multer = require("multer");
 
 const app = express();
 
@@ -15,6 +16,20 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("mongo is running"))
   .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);

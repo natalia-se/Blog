f15374fd797d66aa7374cee09c0cbf3cb4896ab9
natalia-se <flaunder1000@gmail.com>
2022-03-20 20:55:51 +0100

    Error with wrong credentials

--- blog/src/pages/LoginPage.jsx
+++ blog/src/pages/LoginPage.jsx
@@ -5,10 +5,12 @@ import Button from "../components/Button";
 import FormContainer from "../components/FormContainer";
 import Input from "../components/Input";
 import Heading from "../components/Heading";
+import Chip from "../components/Chip";
 
 export default function LoginPage() {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [dataError, setDataError] = useState(null);
 
   const navigate = useNavigate();
 
@@ -24,13 +26,19 @@ export default function LoginPage() {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload),
     })
-      .then((res) => res.json())
+      .then(
+        (res) => res.json()
+        // setResStatus(res.status);
+      )
       .then((data) => {
+        // console.log("data", data.token);
         const token = data.token;
-        localStorage.setItem("Backend1", token);
-        // props.refreshCustomers();
-        // props.refreshMyInfo();
-        navigate("/");
+        if (token) {
+          localStorage.setItem("Backend1", token);
+          navigate("/");
+        }
+
+        if (!token) setDataError(data);
       });
   }
 
@@ -54,6 +62,7 @@ export default function LoginPage() {
           <Button type="submit" value="Login" />
         </form>
       </FormContainer>
+      {dataError && <Chip>{dataError}</Chip>}
     </div>
   );
 }

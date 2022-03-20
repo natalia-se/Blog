6fc675b5848275541e344b397445f54b32ecf062
natalia-se <flaunder1000@gmail.com>
2022-03-20 18:54:10 +0100

    Added styled container

--- blog/src/components/Chip.jsx
+++ blog/src/components/Chip.jsx
@@ -10,8 +10,26 @@ const StyledChip = styled.div`
   border-radius: 25px;
   background-color: #f1f1f1;
   min-height: 50px;
+  min-width: 100px;
+  margin: 15px 15px;
+  // padding: 15px 20px 15px 20px;
+  box-shadow: 0 0 5px #888;
+  text-shadow: 2px 2px 2px #ccc;
 `;
 
 export default function Chip(props) {
   return <StyledChip>{props.children}</StyledChip>;
 }
+
+// border: "1px solid",
+// margin: "15px 0px",
+// padding: "15px 20px 15px 55px",
+// width: "500px",
+// font: "bold 12px verdana",
+// // -moz-box-shadow: 0 0 5px #888;
+// // -webkit-box-shadow: 0 0 5px#888;
+// boxShadow: "0 0 5px #888",
+// textShadow: "2px 2px 2px #ccc",
+// // -webkit-border-radius: 15px;
+// // -moz-border-radius: 15px;
+// borderRadius: "25px",

b87c3525def3b617e3286a7dc841f8c6dcaf82cb
natalia-se <flaunder1000@gmail.com>
2022-03-20 20:09:09 +0100

    file loader

--- blog_backend/index.js
+++ blog_backend/index.js
@@ -1,16 +1,21 @@
 const express = require("express");
+var bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const { authRoute, authorization, requireLogin } = require("./routes/auth");
 const userRoute = require("./routes/users");
 const messageRoute = require("./routes/messages");
 const multer = require("multer");
+const path = require("path");
 
 const app = express();
 
 dotenv.config();
 app.use(express.json());
 app.use(authorization);
+app.use(bodyParser.json({ limit: "50mb" }));
+
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -22,12 +27,12 @@ const storage = multer.diskStorage({
     cb(null, "images");
   },
   filename: (req, file, cb) => {
-    cb(null, req.body.name);
+    cb(null, file.originalname);
   },
 });
 
 const upload = multer({ storage: storage });
-app.post("api/upload", upload.single("file"), (req, res) => {
+app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
 
